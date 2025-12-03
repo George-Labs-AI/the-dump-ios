f30d60489e407f50e62a9cc3ae25e8a0719e31b0
georgeisgreat <130807418+georgeisgreat@users.noreply.github.com>
2025-12-03 14:00:46 -0500

    Initial Commit

---
+++ The_Dump_App_Two/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The_Dump_App_Two
+//
+//  Created by Emily Smith on 12/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The_Dump_App_Two/The_Dump_App_TwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Dump_App_TwoApp.swift
+//  The_Dump_App_Two
+//
+//  Created by Emily Smith on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct The_Dump_App_TwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
